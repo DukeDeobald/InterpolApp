211b85ef98ebf04051f28bb101cd40601a247af4
DukeDeobald <147920932+DukeDeobald@users.noreply.github.com>
2025-06-15 22:41:16 +0100

    Create Suspect.cs

---
+++ Models/Suspect.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace InterpolApp.Models;
+
+public class Suspect
+{
+    public int WantedPersonId { get; set; }
+    public string Forename { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int Weight { get; set; }
+    public double Height { get; set; }
+    public char SexId { get; set; }
+    public string CountryOfBirthId { get; set; }
+    public string NationalityId { get; set; }
+    public string PlaceOfBirth { get; set; }
+    public string DistinguishingMarks { get; set; }
+    public string HairColor { get; set; }
+    public string EyeColor { get; set; }
+    public bool IsDead { get; set; }
+}
