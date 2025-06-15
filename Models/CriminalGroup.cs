720eb3924d9c917b2686b95d19214b9b63e143b8
DukeDeobald <147920932+DukeDeobald@users.noreply.github.com>
2025-06-15 22:40:42 +0100

    Create CriminalGroup.cs

---
+++ Models/CriminalGroup.cs
@@ -0,0 +1,9 @@
+namespace InterpolApp.Models;
+
+public class CriminalGroup
+{
+    public int GroupId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int AssessedQuantity { get; set; }
+}
